9c45b2ff54417efdb49c3555510eeae8026be6ae
Olga Ivanova <olga.ivanova@ambient.digital>
2022-08-24 23:10:25 +0200

    styles: implemented the bestsellers component; styled

--- src/components/Bestsellers.stories.tsx
+++ src/components/Bestsellers.stories.tsx
@@ -1,30 +1,61 @@
+import { withDesign } from "storybook-addon-designs";
+import { Story } from "@storybook/react/types-6-0";
+import { Bestsellers, BestsellersProps } from "./Bestsellers";
 
-import React from 'react';
-import { withDesign } from 'storybook-addon-designs';
-import { Story } from '@storybook/react/types-6-0'
-import { Bestsellers, BestsellersProps } from './Bestsellers';
-
+// component level
 export default {
-  title: 'Component/Bestsellers',
+  title: "Component/Bestsellers",
   component: Bestsellers,
   decorators: [withDesign],
+  // Default args that will be applied
+  // in case no custom args are specified.
+  args: {
+    label: "Bestsellers",
+    image: "",
+    name: "Sneakers",
+    brand: "Nike",
+    price: "100",
+  },
   argTypes: {},
-  
+
   parameters: {
     design: {
-      type: 'image',
-      url: 'https://res.cloudinary.com/elie-tech/image/upload/v1604444026/frontwork-prod/40:2.png',
+      type: "image",
+      url:
+        "https://res.cloudinary.com/elie-tech/image/upload/v1604444026/frontwork-prod/40:2.png",
     },
   },
-
 };
 
+// story level
 const Template: Story<BestsellersProps> = (args: BestsellersProps) => {
-  return <Bestsellers {...args} />
-}
+  return <Bestsellers {...args} />;
+};
 
 export const Basic = Template.bind({});
 Basic.args = {
-  label: 'Bestsellers',
+  products: [
+    {
+      name: "Nike Air Max 270",
+      brand: "Nike",
+      image:
+        "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto,q_auto:eco/15989f45-9d49-4c9a-8e02-f9ed1d8a0aa0/free-metcon-4-trainingsschuh-TQMCZg.png",
+      price: "195.80",
+    },
+    {
+      name: "Nike Air Max 90",
+      brand: "Nike",
+      image:
+        "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/830d2ce1-2166-4777-ac99-acddd3639e82/tiempo-legend-9-elite-fg-fussballschuh-fur-normalen-rasen-fFqX06.png",
+      price: "195.80",
+    },
+    {
+      name: "Nike Air Max Plus",
+      brand: "Nike",
+      image:
+        "https://static.nike.com/a/images/t_PDP_1280_v1/f_auto/e72c2c6d-eaf7-4553-b67a-8c27ec96f062/free-metcon-4-trainingsschuhe-x7P8BQ.png",
+      price: "195.80",
+    },
+  ],
 };
-  
+Basic.parameters = {};
--- src/components/Bestsellers.tsx
+++ src/components/Bestsellers.tsx
@@ -1,11 +1,124 @@
+import React from "react";
+import styled from "styled-components";
 
-import React from 'react';
+const Canvas = styled.section`
+  font-family: Verdana, Arial, Helvetica, sans-serif;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: #faefe3;
+  width: 22rem;
+  height: 30rem;
+  border-radius: 1rem;
+  padding: 6rem 8rem;
+  box-shadow: 0 2px 20px rgba(0, 0, 0, 0.2);
+  margin: 4rem auto;
+`;
+
+const ProductsWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  border-radius: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+`;
+
+const ProductsContent = styled.div`
+  background-color: rgb(255, 255, 255);
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  height: 100%;
+  border-radius: 1rem;
+  padding: 2rem 2rem;
+  min-width: 20rem;
+  gap: 1rem;
+`;
+
+const Label = styled.p`
+  align-self: flex-start;
+  margin: 0;
+  font-size: 1.5rem;
+  font-weight: 500;
+`;
+
+// Content of each cart
+const ProductWrapper = styled.div`
+  display: flex;
+  background-color: rgb(254, 242, 238);
+  width: 100%;
+  border-radius: 1rem;
+  gap: 2rem;
+  justify-content: space-between;
+  height: 7rem;
+  box-shadow: rgba(0, 0, 0, 0.1) -4px 9px 25px -6px;
+`;
+
+const ProductImage = styled.img`
+  height: 100%;
+  width: 7rem;
+  height: 7rem;
+  object-fit: cover;
+`;
+
+const ProductInfo = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 1rem;
+
+  h5,
+  p {
+    margin: 0;
+  }
+`;
+const ProductName = styled.h5`
+  color: rgb(254, 128, 92);
+`;
+const ProductBrand = styled.p`
+  font-size: 0.8rem;
+  color: rgb(202, 196, 196);
+`;
+const ProductPrice = styled.p`
+  font-size: 0.8rem;
+  font-weight: 500;
+`;
 
 export interface BestsellersProps {
-  label: string
+  label: string;
+  products: {
+    image: string;
+    name: string;
+    brand: string;
+    price: string;
+  }[];
 }
 
-export const Bestsellers: React.FC<BestsellersProps> = props => {
-  return <div>{props.label}</div>;
-}
-  
+export const Bestsellers: React.FC<BestsellersProps> = ({
+  label,
+  products,
+  ...props
+}: BestsellersProps) => {
+  return (
+    <Canvas>
+      <ProductsWrapper>
+        <ProductsContent>
+          <Label>{label}</Label>
+          {(products || []).map((product) => (
+            <ProductWrapper>
+              <ProductImage src={product.image} alt="sneakers" />
+              <ProductInfo>
+                <ProductName>{product.name}</ProductName>
+                <ProductBrand>{product.brand}</ProductBrand>
+                <ProductPrice>${product.price}</ProductPrice>
+              </ProductInfo>
+            </ProductWrapper>
+          ))}
+        </ProductsContent>
+      </ProductsWrapper>
+    </Canvas>
+  );
+};
